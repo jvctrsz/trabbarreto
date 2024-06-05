7fffb7a2a27d25c6f68b086a713deb1499ead7a7
João Victor Matias <108106191+jvctrsz@users.noreply.github.com>
2024-06-04 22:06:20 -0300

    active

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,29 @@
-let homePage = document.querySelectorAll('#homePage')
-let background = document.querySelectorAll('#backgroundHome')
+let loginId = document.querySelector('#loginD')
+let homePage = document.querySelector('#homePage')
+let background = document.querySelector('.section-display')
+let backProd = document.querySelector('.background-produtos')
+let backMovi = document.querySelector('.background-movimentacao')
+
+
+function activeCheck() {
+    let activeElement = document.querySelector('.active');
+    if (activeElement) {
+        homePage.classList.add("active")
+        activeElement.classList.remove("active");
+    }
+}
+
+function produtos(){
+    activeCheck()
+    backProd.classList.add("active")
+}
+
+function movimentacao(){
+    backMovi.classList.add('active')
+    activeCheck()
+}
+
+function login(){
+    loginId.classList.remove("active")
+    homePage.classList.add("active")
+}
