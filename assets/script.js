f45e6319825758731c7550564967b583b6ac12ca
João Victor Matias <108106191+jvctrsz@users.noreply.github.com>
2024-06-05 21:53:08 -0300

    visual

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,10 @@ let homePage = document.querySelector('#homePage')
 let background = document.querySelector('#background')
 let backProd = document.querySelector('.background-produtos')
 let backMovi = document.querySelector('.background-movimentacao')
+let incluirprod = document.querySelector('#incluirProd')
+let incluirmov = document.querySelector('#incluirMov')
+let fecharprod = document.querySelector('#fechar-prod')
+let fecharmov = document.querySelector('#fechar-mov')
 
 
 function activeCheck() {
@@ -27,3 +31,21 @@ function login(){
     homePage.classList.add("activeHome")
     background.classList.add("active")
 }
+
+function incluirProd(){
+    incluirprod.classList.add('incluirAct')
+
+}
+
+function incluirMov(){
+    incluirmov.classList.add('incluirAct')
+}
+
+function fecharProd(){
+    incluirprod.classList.remove('incluirAct')
+}
+
+function fecharMov(){
+    incluirmov.classList.remove('incluirAct')
+}
+
